ed1bf3508482a41a8a0f9a8ded587740807230d9
morsalinamunmun <morsalinamunmun@gmail.com>
2023-11-07 16:42:35 +0600

    community section add

---
+++ src/components/Community/Community.jsx
@@ -0,0 +1,45 @@
+
+const Community = () => {
+    return (
+        <div className="py-10 mb-10">
+            <div className="text-center space-y-5">
+                <h2 className="text-orange-500 text-2xl font-bold ">Our Community</h2>
+                <p className="">Our community are best services for food donation</p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
+                <div className="bg-base-100 p-10 shadow-xl text-center space-y-3">
+                    <img className="w-14 flex mx-auto rounded-full" src="https://i.ibb.co/mcVHCGp/men.jpg" alt="" />
+                    <h2 className="text-orange-500 font-semibold text-xl">Jolekha Banu</h2>
+                    <h2 className="text-orange-500">Team Leader</h2>
+                    <p>Oversee the team's activities and set clear goals.
+                        Coordinate and schedule team meetings and activities.
+                        Ensure that team members are working effectively.
+                        Handle any challenges or issues within the team.
+                        Report on the team's progress and impact.</p>
+                </div>
+                <div className="bg-base-100 p-10 shadow-xl text-center space-y-3">
+                    <img className="w-14 flex mx-auto rounded-full" src="https://i.ibb.co/mcVHCGp/men.jpg" alt="" />
+                    <h2 className="text-orange-500 font-semibold text-xl">Jolekha Banu</h2>
+                    <h2 className="text-orange-500">Team Leader</h2>
+                    <p>Oversee the team's activities and set clear goals.
+                        Coordinate and schedule team meetings and activities.
+                        Ensure that team members are working effectively.
+                        Handle any challenges or issues within the team.
+                        Report on the team's progress and impact.</p>
+                </div>
+                <div className="bg-base-100 p-10 shadow-xl text-center space-y-3">
+                    <img className="w-14 flex mx-auto rounded-full" src="https://i.ibb.co/mcVHCGp/men.jpg" alt="" />
+                    <h2 className="text-orange-500 font-semibold text-xl">Jolekha Banu</h2>
+                    <h2 className="text-orange-500">Team Leader</h2>
+                    <p>Oversee the team's activities and set clear goals.
+                        Coordinate and schedule team meetings and activities.
+                        Ensure that team members are working effectively.
+                        Handle any challenges or issues within the team.
+                        Report on the team's progress and impact.</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Community;
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from "../../Banner/Banner";
+import Community from "../../Community/Community";
 import Foods from "../../Foods/Foods";
 import GiveMonthly from "../../GiveMonthly/GiveMonthly";
 import Footer from "../../Shared/Footer/Footer";
@@ -12,6 +13,7 @@ const Home = () => {
              <Banner></Banner>
              <Foods></Foods>
              <GiveMonthly></GiveMonthly>
+             <Community></Community>
              <Footer></Footer>
         </div>
     );
